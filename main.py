c86677fe44604478d0c2fa6483b95ea981439700
mo3tazJ <en.mo3taz@gmail.com>
2026-02-02 13:16:46 +0300

    update main with routes

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
+
 app = FastAPI()
 
-
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Hello World!"
-    }
-
-@app.get("/about")
-def about():
-    return {
-        "message": "About-US"
-    }
+app.include_router(base.base_router)
