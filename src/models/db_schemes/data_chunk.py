593002b90aa0e80faffd80404e5646defeb687b9
mo3tazJ <en.mo3taz@gmail.com>
2026-02-08 23:22:40 +0300

    create db schemes

---
+++ src/models/db_schemes/data_chunk.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+from bson.objectid import ObjectId
+
+
+class DataChunk(BaseModel):
+    id: Optional[ObjectId] = Field(None, alias="_id")
+    chunk_text: str = Field(..., min_length=1)
+    chunk_metadata: dict
+    chunk_order: int = Field(..., gt=0)
+    chunk_project_id: ObjectId
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    # class config:
+    #     arbitrary_types_allowed=True
+    
+
+
