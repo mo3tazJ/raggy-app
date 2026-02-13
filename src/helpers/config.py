ed3ab3d9c3f1e585b1610e55704f34c0b5798e6e
mo3tazJ <en.mo3taz@gmail.com>
2026-02-13 15:17:59 +0300

    update config to add all new .env values

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,20 +1,36 @@
-from pydantic_settings import BaseSettings ,SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    
+
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+
     MONGODB_URL: str
     MONGODB_DATABASE: str
 
-    
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None  # type: ignore
+    OPENAI_API_URL: str = None  # type: ignore
+    COHERE_API_KEY: str = None  # type: ignore
+
+    GENERATION_MODEL_ID: str = None  # type: ignore
+    EMBEDDING_MODEL_ID: str = None  # type: ignore
+    EMBEDDING_MODEL_SIZE: int = None  # type: ignore
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None  # type: ignore
+    GENERATION_DEFAULT_MAX_TOKENS: int = None  # type: ignore
+    GENERATION_DEFAULT_TEMPERATURE: float = None  # type: ignore
+
     model_config = SettingsConfigDict(env_file=".env")
 
-    
+
 def get_settings():
-    return Settings() # type: ignore
+    return Settings()  # type: ignore
